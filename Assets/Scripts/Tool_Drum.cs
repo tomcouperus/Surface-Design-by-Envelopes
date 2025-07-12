4bfc3f9d4d6d732b25f935a319cdb8637a8c42ae
tom couperus <tcouperus@hotmail.nl>
2025-07-12 12:45:04 +0200

    Get half-drum tool to work

--- Assets/Scripts/Tool_Drum.cs
+++ Assets/Scripts/Tool_Drum.cs
@@ -5,60 +5,66 @@ public class Tool_Drum : Tool
     [SerializeField]
     private float radius = 0.5f;
     [SerializeField]
-    private float circleRadius = 5;
+    private float rho = 5;
 
     // Helper values
-    private float R { get { return circleRadius; } }
-    private float B { get { return R - Mathf.Sqrt(R * R - height * height / 4); } }
-    private float R_b { get { return radius; } }
-    private float R_c { get { return R_b + B; } }
-    private float V { get { return R - R_c; } }
+    private float D { get { return rho - radius; } }
 
     private float GetAngleAt(float a)
     {
-        return Mathf.Asin(a / R);
+        float term = (a * height) / (2 * rho);
+        return Mathf.Asin(term);
     }
 
     private float GetAngleDaAt(float a)
     {
-        throw new System.NotImplementedException();
+        float term = (a * height) / (2 * rho);
+        float term_a = height / (2 * rho);
+        return term_a / Mathf.Sqrt(1 - term * term);
 
     }
 
     public override float GetRadiusAt(float a)
     {
         float theta = GetAngleAt(a);
-        float sphRad = GetSphereRadiusAt(a);
-        return sphRad * Mathf.Cos(theta);
+        return GetSphereRadiusAt(a) * Mathf.Cos(theta);
     }
 
     public override float GetRadiusDaAt(float a)
     {
-        throw new System.NotImplementedException();
-
+        float theta = GetAngleAt(a);
+        float theta_a = GetAngleDaAt(a);
+        float c_theta = Mathf.Cos(theta);
+        float s_theta = Mathf.Sin(theta);
+        return GetSphereRadiusDaAt(a) * c_theta +
+               GetSphereRadiusAt(a) * -s_theta * theta_a;
     }
 
     public override float GetSphereCenterHeightAt(float a)
     {
         float theta = GetAngleAt(a);
-        return V * Mathf.Tan(theta);
+        return D * Mathf.Tan(theta);
     }
 
     public override float GetSphereCenterHeightDaAt(float a)
     {
-        throw new System.NotImplementedException();
-
+        float theta = GetAngleAt(a);
+        float theta_a = GetAngleDaAt(a);
+        float c_theta = Mathf.Cos(theta);
+        return D / (c_theta * c_theta) * theta_a;
     }
 
     public override float GetSphereRadiusAt(float a)
     {
         float theta = GetAngleAt(a);
-        return R - V / Mathf.Cos(theta);
+        return rho - D / Mathf.Cos(theta);
     }
 
     public override float GetSphereRadiusDaAt(float a)
     {
-        throw new System.NotImplementedException();
+        float theta = GetAngleAt(a);
+        float theta_a = GetAngleDaAt(a);
+        return -D * Mathf.Tan(theta) / Mathf.Cos(theta) * theta_a;
 
     }
 
@@ -67,7 +73,7 @@ public class Tool_Drum : Tool
         float toolRad = GetRadiusAt(a);
         return new Vector3(
             toolRad * Mathf.Cos(tRad),
-            a * height + height / 2,
+            a * height / 2,
             toolRad * Mathf.Sin(tRad)
         );
     }
